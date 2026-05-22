1258df7209e2c438b12974339867687439d544bd
guadalupepereyra192-cloud <guadalupepereyra192@gmail.com>
2026-05-22 19:49:52 -0300

    actualizo web completa

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,10 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import './index.css';
-import ChispaLanding from './App.jsx';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import ChispaLanding from "./App.jsx";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(<ChispaLanding />);
+createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <ChispaLanding />
+  </React.StrictMode>
+);
